87fa8f9b54241d6ec3efc8a45756294bd847c074
Singelé Cédric <cedric.singele@ifosup.wavre.be>
2024-11-22 20:45:27 +0100

    Bug correction User.find().skip() not return the correct amount of user DEFAULT_PAGE must be 0.

--- server/src/services/user.service.ts
+++ server/src/services/user.service.ts
@@ -17,10 +17,10 @@ async function getAll(pagination: Pagination, sort: Sort | null) {
     try {
         const start = pagination.start || DEFAULT_PAGE;
         const size = pagination.size || DEFAULT_SIZE;
-        if(sort) {
-            return await User.find().sort({[`${sort?.id}`]: sort?.desc ? "desc" : "asc"}).skip(start).limit(size);    
+        if (sort) {
+            return await User.find().sort({ [`${sort?.id}`]: sort?.desc ? "desc" : "asc" }).skip(start).limit(size);
         }
-        return await User.find().skip(start).limit(size);
+        return await User.find().skip(start - 1).limit(size);
     } catch (err: any) {
         throw new Error("Server error");
     }
@@ -40,7 +40,7 @@ async function create(user: TUser): Promise<TUser | ValidationError | Array<Vali
 
         return await userToSave.save();
     } catch (err: any) {
-        let errors:Array<ValidationError> = [];
+        let errors: Array<ValidationError> = [];
         if (err.code = errorsCode.DUPLICATE_KEY_EXCEPTION) {
             const v = transformKeyValueToErrorFields(err.keyValue);
             v.errorFields.forEach(errorField => {
